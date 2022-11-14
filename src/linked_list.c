1159e592d7068d87cc34b625a0391d5b5a748dd7
AndreasGBP <114382655+AndreasGBP@users.noreply.github.com>
2022-11-14 15:23:10 +0100

    WIP

--- src/linked_list.c
+++ src/linked_list.c
@@ -25,21 +25,32 @@ void free_list(node *p) {
 /* print list to console */
 void print_list(node *p) {
   // Add your code for exercise 1
+  if(p == NULL) //base case
+    return;
+  printf("%d ", p -> value); //prints element
+  print_list(p -> next); //recursive step
   // There is NO testcode for this
 }
 
+int sum = 0;
+
 int sum_squares(node *p) {
-  // Add your code for excercise 2
+  // Add your code for excercise 2 
+  if(p == NULL) //base case
+    return 0;
+  return p->value * p->value + sum_squares(p -> next); //recursive step
   // You can find the tests in tests.cpp
-  return -1;
 }
 
 typedef int (*fn_int_to_int)(int);
 
 node *map(node *p, fn_int_to_int f) { 
-  // Add your code for excercise 3
-  
-  return NULL; 
+  // Add your code for excercise 
+  if(p == NULL) //base case
+    return 0;
+  return make_node(f(p->value), map(p->next,f)); 
+  //recursive step, it creates a new linked list using the make_node function and maps the values of p with the function f
+
 }
 
 int square(int x) { return x * x; }
--- src/main.c
+++ src/main.c
@@ -2,10 +2,60 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "linked_list.h"
+#include "btree.h"
 
 // File for sandboxing and trying out code
 int main(int argc, char **argv) {
   // Add your code
+  /*
+  node *ns = make_node(
+        1, make_node(2, make_node(3, make_node(4, make_node(5, NULL)))));
+  node *new = map(ns, square);
+  print_list(ns);
+  printf("\n");
+  print_list(new);
+  printf("%d", sum_squares(ns));
+  */
+    struct tree_node *root = NULL;
 
-  return 0;
+  root = Initialize(root);
+  root = Insert(20, root);
+  root = Insert(5, root);
+  root = Insert(1, root);
+  root = Insert(15, root);
+  root = Insert(9, root);
+  root = Insert(7, root);
+  root = Insert(12, root);
+  root = Insert(30, root);
+  root = Insert(25, root);
+  root = Insert(40, root);
+  root = Insert(45, root);
+  root = Insert(42, root);
+
+  root = Insert(3, root);
+
+  root = Remove(3, root);
+  root = Insert(-1, root);
+  root = Insert(-1, root);
+  root = Remove(-1, root);
+  root = Remove(-1, root);
+  root = Remove(45, root);
+  root = Remove(42, root);
+  root = Insert(16, root);
+  root = Remove(7, root);
+
+  root = Remove(1, root);
+  root = Remove(7, root);
+  root = Remove(12, root);
+  root = Remove(9, root);
+  root = Remove(15, root);
+  root = Remove(5, root);
+  root = Remove(42, root);
+  root = Remove(45, root);
+  root = Remove(25, root);
+  root = Remove(40, root);
+  root = Remove(30, root);
+  root = Remove(20, root);
+  root = Remove(16, root);
 }
