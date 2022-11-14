b556a6ac1207b8ca7482f4fb9f02c03e77cd533e
AndreasGBP <114382655+AndreasGBP@users.noreply.github.com>
2022-11-14 15:40:30 +0100

    Final

--- src/btree.c
+++ src/btree.c
@@ -6,9 +6,9 @@
 #include <stdlib.h>
 
 struct tree_node *Insert(int x, struct tree_node *t) {
-  // Insert item x into the tree t
+  // Insert item "x" into the tree "t"
   tree_node *new_node = malloc(sizeof(tree_node));
-  if(t == NULL) //base case
+  if(t == NULL) //Base case
   {
     t = new_node;
     t -> item = x;
@@ -25,33 +25,33 @@ struct tree_node *Insert(int x, struct tree_node *t) {
     {
       t->left = Insert(x, t -> left);
     }
-  } //two recursive steps for when x is going on the right side of tree and when going on the left side
+  } //The two recursive steps for when "x" is going on the right side of tree, and when it is going on the left side
   return t;
 }
 
 struct tree_node *Remove(int x, struct tree_node *t) {
-  // Remove one item from the tree t
+  // Remove one item from the tree "t"
   if(Contains(x,t) == 0)
   {
     return t;
-  } //Checks if x is in the tree, returns t if not
+  } //Checks if "x" is in the tree, returns "t" if not
   if(x == t -> item)
   {
     tree_node *temp = t;
     if(t -> right != NULL)
-      temp = temp -> right; //sets temp pointer to right child if exists
+      temp = temp -> right; //Sets the pointer temp to right child, if it exists
     tree_node *temp2;
     if(t -> left == NULL && t -> right == NULL)
     {
       t = NULL;
       return t;
-    } //if tree is empty besides root it sets root to NULL, back to initialized state
+    } //If tree is empty besides root it sets root to NULL, back to initialized state
     while(temp -> left != NULL) 
       {
         temp2 = temp;
         temp = temp -> left; 
-      } //loops through tree through left children until end of sequence
-      t -> item = temp -> item; //sets root item to the lowest number on the right side of the tree
+      } //Loops through the tree through left children, until end of sequence
+      t -> item = temp -> item; //Sets root item to the lowest number, on the right side of the tree
       temp2 -> left = NULL;
       free(temp);
       return t;
@@ -73,10 +73,10 @@ struct tree_node *Remove(int x, struct tree_node *t) {
       temp = temp->left;
       count = 0;
     }
-  } //this loops throught the tree until temp -> item is equal to x
+  } //This loops through the tree, until temp -> item is equal to "x"
   
 
-  if(temp -> left == NULL && temp -> right == NULL) //special case if the number to be removed is already a leaf
+  if(temp -> left == NULL && temp -> right == NULL) //Special case if the number to be removed, is already a leaf
   {
     if(count == 1){
       temp1 -> right = NULL;
@@ -91,11 +91,11 @@ struct tree_node *Remove(int x, struct tree_node *t) {
 
   tree_node *temp2 = temp;
   tree_node *temp3 = temp;
-  if(x < t ->item){ //case for when on the left side of root
+  if(x < t ->item){ //Case for when on the left side of root
     if(temp2 -> right != NULL)
     {
       temp2 = temp2 -> right;
-      if(temp2 -> left == NULL) //special case if temp -> right is a leaf
+      if(temp2 -> left == NULL) //Special case if temp -> Right is a leaf
       {
         temp -> item = temp2 -> item;
         temp3 -> right = NULL;
@@ -107,17 +107,17 @@ struct tree_node *Remove(int x, struct tree_node *t) {
       {
         temp3 = temp2;
         temp2 = temp2 ->left;
-      } //loops through tree via left children
+      } //Loops through the tree via the left children
     temp3 -> left = NULL;
     temp -> item = temp2 -> item;
     free(temp2);
     return t;
   }
-  else { //case for when on the right side of tree
+  else { //Case for when on the right side of tree
     if(temp2 -> left != NULL)
     {
       temp2 = temp2 -> left;
-      if(temp2 -> left == NULL) //special case if temp -> left is a leaf
+      if(temp2 -> left == NULL) //Special case if temp -> left is a leaf
       {
         temp -> item = temp2 -> item;
         temp3 -> left = NULL;
@@ -129,7 +129,7 @@ struct tree_node *Remove(int x, struct tree_node *t) {
       {
         temp3 = temp2;
         temp2 = temp2 ->right;
-      } //loops through via left children
+      } //Loops through, via left children
     temp3 -> right = NULL;
     temp -> item = temp2 -> item;
     free(temp2);
@@ -139,15 +139,15 @@ struct tree_node *Remove(int x, struct tree_node *t) {
 
 int Contains(int x, struct tree_node *t) {
 
-  // Return true/1 if the tree t contains item x. Return false/0 otherwise.
-  if(t == NULL) //base case 0 if we hit leaf
+  // Return 1 if the tree "t" contains item "x". Return 0 otherwise.
+  if(t == NULL) //Base case 0 if we hit leaf
     return 0;
-  if(t -> item == x)//base case 1 if x is found
+  if(t -> item == x)//Base case 1 if x is found
     return 1;
 
-  else if(x > t -> item) //recursive step for x > item
+  else if(x > t -> item) //Recursive step for x > item
     Contains(x, t -> right);
-  else if(x < t -> item) //recursive step for x < item
+  else if(x < t -> item) //Recursive step for x < item
     Contains(x, t -> left);
 }
 
--- src/linked_list.c
+++ src/linked_list.c
@@ -4,7 +4,7 @@
 
 #include "linked_list.h"
 
-/* functions to create lists */
+/* Functions to create the lists */
 node *make_node(int v, node *q) {
   node *p = malloc(sizeof(node));
   p->value = v;
@@ -12,7 +12,7 @@ node *make_node(int v, node *q) {
   return p;
 }
 
-/* free all nodes in the list p */
+/* Free all the nodes in the list p */
 void free_list(node *p) {
   node *q;
   while (p != NULL) {
@@ -22,13 +22,13 @@ void free_list(node *p) {
   }
 }
 
-/* print list to console */
+/* Print the list to console */
 void print_list(node *p) {
   // Add your code for exercise 1
-  if(p == NULL) //base case
+  if(p == NULL) //Base case
     return;
-  printf("%d ", p -> value); //prints element
-  print_list(p -> next); //recursive step
+  printf("%d ", p -> value); //Prints element
+  print_list(p -> next); //Recursive step
   // There is NO testcode for this
 }
 
@@ -38,7 +38,7 @@ int sum_squares(node *p) {
   // Add your code for excercise 2 
   if(p == NULL) //base case
     return 0;
-  return p->value * p->value + sum_squares(p -> next); //recursive step
+  return p->value * p->value + sum_squares(p -> next); //Recursive step
   // You can find the tests in tests.cpp
 }
 
@@ -46,7 +46,7 @@ typedef int (*fn_int_to_int)(int);
 
 node *map(node *p, fn_int_to_int f) { 
   // Add your code for excercise 
-  if(p == NULL) //base case
+  if(p == NULL) //Base case
     return 0;
   return make_node(f(p->value), map(p->next,f)); 
   //recursive step, it creates a new linked list using the make_node function and maps the values of p with the function f
